b8473655f5780cb85c7b2ccf6cfca7ca63224bd5
Mark Lubin <mark@Marks-MacBook-Pro.local>
2025-02-24 13:42:12 -0800

    Initial Commit

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+from flask import Flask
+def create_app():
+    app = Flask(__name__)
+    from app.routes import api_blueprint
+    app.register_blueprint(api_blueprint, url_prefix='/api')
+    return app
+
